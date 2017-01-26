4d3c03927bde146dc8648cd9bc08edef9d58d0c8
Paul Chin <pchinjr@gmail.com>
2017-01-26 16:26:28 -0500

    em vee pee

--- js/main.js
+++ js/main.js
@@ -21,9 +21,45 @@ $("#quoteForm").submit(function(event){
 
 
 var calculate = function(obj) {
-    console.log(obj.numberTransactions);
+    let userScore = 0;  
 
+    if(obj.companyType === "Sole Proprietor"){
+        userScore += 3;
+    }
 
-    return 
+    if(obj.operatingYears === "Under A Year"){
+        userScore += 6;
+    } else if (obj.operatingYears === "1-3 years") {
+        userScore += 3;
+    } else {
+        userScore += 0;
+    }
+
+    if(obj.seasonal === "Yes") {
+        userScore += 8;
+    }
+
+    if(obj.deposits === "Yes") {
+        userScore += 10;
+    }
+
+    const salesInt = parseInt(obj.salesVolume);
+    if(obj.salesVolume <= 50000) {
+        userScore += 0;
+    } else if(obj.salesVolume >= 50001 && obj.salesVolume <= 300000) {
+        userScore += 1;
+    } else if(obj.salesVolume >= 300001 && obj.salesVolume <= 2000000) {
+        userScore += 2;
+    } else if (obj.salesVolume >= 2000001) {
+        userScore +=3;
+    }
+
+    const transactionsInt = parseInt(obj.numberTransactions);
+
+    userScore = userScore / 5; //averages users score out of 5 questions
+
+    document.getElementById('#modal').innerHTML = `Congratulations ${obj.firstName} Your Score is ${userScore} and your average sale is ${salesInt / transactionsInt}`;
+
+    $('#myModal').modal();
 }
 
